d0e411eda91c6661de2f674054fa29c3beece913
Gustavo Rodrigues <gsrodrigues280@gmail.com>
2022-05-01 00:08:58 -0300

    comments

--- mongoDB/handlers/users.go
+++ mongoDB/handlers/users.go
@@ -10,14 +10,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-// type MongoController struct {
-// 	session *mongo.Client
-// }
-
-// func NewMongoController(session *mongo.Client) *MongoController {
-// 	return &MongoController{session}
-// }
-
+// GetUsers will return all the users
 func GetUsers(ctx *gin.Context) {
 	response, err := models.GetUsers(ctx)
 	if err != nil {
@@ -29,12 +22,20 @@ func GetUsers(ctx *gin.Context) {
 	if err = response.All(ctx, &allusers); err != nil {
 		log.Println(err.Error())
 	}
+	// send the response message
 	ctx.JSON(http.StatusOK, allusers[0])
 }
 
+// GetUser will return a specific user
 func GetUser(ctx *gin.Context) {
+	// get the userID from the ctx params, key is "id"
 	userID := ctx.Param("id")
+	if userID == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "no user ID was provided"})
+		return
+	}
 
+	// call GetUser to get the user
 	user, err := models.GetUser(userID)
 	if err != nil {
 		if err.Error() == "mongo: no documents in result" {
@@ -44,56 +45,72 @@ func GetUser(ctx *gin.Context) {
 		}
 		return
 	}
+	// send the response message
 	ctx.JSON(http.StatusOK, user)
 }
 
+// CreateUser create a user in the postgres database
 func CreateUser(ctx *gin.Context) {
+	// create an empty user of type entity.User
 	var user entity.User
 
+	// decode the json request to user
 	if err := ctx.BindJSON(&user); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
+	// call CreateUser to create the user
 	if err := models.CreateUser(user); err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	// send the response message
 	ctx.JSON(http.StatusCreated, gin.H{"success": "User was successfully created"})
 }
 
+// UpdateUser update a user in the postgres database
 func UpdateUser(ctx *gin.Context) {
+	// create an empty user of type entity.User
 	var user entity.User
 
+	// get the userID from the ctx params, key is "id"
 	userID := ctx.Param("id")
 	if userID == "" {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "no user ID was provided"})
 		return
 	}
 
+	// decode the json request to user
 	if err := ctx.BindJSON(&user); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
+	// call UpdateUser to update the user
 	if err := models.UpdateUser(userID, user); err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	// send the response message
 	ctx.JSON(http.StatusOK, gin.H{"success": "user was successfully updated"})
 }
 
+// DeleteUser delete a user in the postgres database
 func DeleteUser(ctx *gin.Context) {
 
+	// get the userID from the ctx params, key is "id"
 	userID := ctx.Param("id")
 	if userID == "" {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "no user ID was provided"})
 		return
 	}
 
+	// call DeleteUser to delete the user
 	if err := models.DeleteUser(userID); err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	// send the response message
 	ctx.JSON(http.StatusOK, gin.H{"success": "User was successfully deleted"})
 }
