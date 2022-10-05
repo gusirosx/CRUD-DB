a6919b52ef75951dcf7acda64f39f595c2bef41d
Gustavo Rodrigues <gsrodrigues280@gmail.com>
2022-10-04 22:04:50 -0300

    domain

---
+++ clean/domain/user.go
@@ -0,0 +1,12 @@
+package domain
+
+/*
+Domain encapsulate enterprise wide business rules.
+*/
+
+type User struct {
+	Id     string `json:"id" bson:"_id"`
+	Name   string `json:"name" bson:"name"`
+	Gender string `json:"gender" bson:"gender"`
+	Age    int    `json:"age" bson:"age"`
+}
