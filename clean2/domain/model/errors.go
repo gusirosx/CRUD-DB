ccbf59d3bf96f2b21535536d50ff7a53595b7c24
Gustavo Rodrigues <gsrodrigues280@gmail.com>
2022-10-24 22:52:41 -0300

    remove clean

--- clean2/domain/model/errors.go
+++
@@ -1,6 +0,0 @@
-package model
-
-import "errors"
-
-// ErrNotFound not found
-var ErrNotFound = errors.New("not found")
--- clean2/usecase/presenter.go
+++
@@ -1,8 +0,0 @@
-package usecase
-
-import "clean2/domain/model"
-
-type UserPresenter interface {
-	ResponseUsers(u []*model.User) []*model.User
-	ResponseUser(u *model.User) *model.User
-}
